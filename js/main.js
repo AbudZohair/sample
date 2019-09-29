3a7b09369aef80e2ec48379819842fbe3ce35869
Abud Zohair <abud.zohair@gmail.com>
2019-09-29 09:23:54 +0200

    testnetlify

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+
+
+// Sticky menu background
+window.addEventListener('scroll', ()=>{
+  if(scrollY > 150){
+    document.querySelector('nav').style.opacity = 0.9;
+  }
+  else{
+    document.querySelector('nav').style.opacity = 1;
+
+  }
+});
+
+
+
+// Smooth Scrolling
+$('nav a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
